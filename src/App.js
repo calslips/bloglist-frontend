676fa20277bd0b1e5802f6570487518548ce9ede
Caleb Phillips <csphillips5@gmail.com>
2021-09-13 09:32:31 -0400

    5.2 utilize localStorage for persistent login & add ability to logout

--- src/App.js
+++ src/App.js
@@ -15,17 +15,34 @@ const App = () => {
     });
   }, []);
 
+  useEffect(() => {
+    const loggedInUserJSON = window.localStorage.getItem('loggedInUser');
+    if (loggedInUserJSON) {
+      const user = JSON.parse(loggedInUserJSON);
+      setUser(user);
+    }
+  }, []);
+
   const handleLogin = async (event) => {
     event.preventDefault();
 
     try{
       const user = await loginService.login({ username, password });
+
+      window.localStorage.setItem(
+        'loggedInUser', JSON.stringify(user)
+      );
       setUser(user);
       setUsername('');
       setPassword('');
     } catch (exception) {
       console.log(exception);
     }
+  };
+
+  const handleLogout = (event) => {
+    window.localStorage.removeItem('loggedInUser');
+    setUser(null);
   };
 
   return (
@@ -57,7 +74,7 @@ const App = () => {
         </>
       : <>
           <h2>blogs</h2>
-          <p>{user.name} logged in</p>
+          <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
           {blogs.map((blog) =>
            <Blog key={blog.id} blog={blog} />
           )}
