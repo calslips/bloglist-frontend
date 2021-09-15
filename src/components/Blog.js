355b12010fc55ba26dc6f979f4b6b3f39b2b10ef
Caleb Phillips <csphillips5@gmail.com>
2021-09-15 19:46:16 -0400

    5.7 add view toggle for blogs to alternate displaying full info/partial

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [displayInfo, setDisplayInfo] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 5,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    borderRadius: 3,
+    marginBottom: 5
+  };
+
+  return (
+    <div style={blogStyle}>
+      {displayInfo
+        ? <>
+          <p>{blog.title} - {blog.author} <button onClick={() => setDisplayInfo(false)}>hide</button></p>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>like</button></p>
+          <p>{blog.user.name}</p>
+          </>
+        : <p>{blog.title} - {blog.author} <button onClick={() => setDisplayInfo(true)}>view</button></p>
+      }
+    </div>
+  );
+};
 
 export default Blog
