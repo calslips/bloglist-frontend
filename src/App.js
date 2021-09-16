4903e32bd54ef61e92238efb83c81185c5b3ad85
Caleb Phillips <csphillips5@gmail.com>
2021-09-16 01:37:20 -0400

    5.9 sort blog posts by number of likes

--- src/App.js
+++ src/App.js
@@ -67,9 +67,10 @@ const App = () => {
             notification={noticeContent}
             addBlog={addBlog}
           />
-          {blogs.map((blog) =>
-           <Blog key={blog.id} blog={blog} />
-          )}
+          {blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map((blog) => <Blog key={blog.id} blog={blog} />)
+          }
         </>
     }
     </div>
